9863ebef28a8f65d2b587a467cb2a883c520e712
Bastian Gebhardt <Bastian.Gebhardt@jambit.com>
2024-08-09 23:56:46 +0200

    add grouped examples

--- examples/eslint7/.eslintrc.js
+++ examples/eslint7/.eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
         'jest-mock-config/jest-mock-without-import': 'error',
         'jest-mock-config/jest-mocked-without-mock': 'error',
         'jest-mock-config/jest-mock-multiple-with-same-path': 'error',
+        'jest-mock-config/jest-mock-grouped': 'error',
+        'jest-mock-config/jest-mocked-grouped': 'error',
+        'jest-mock-config/jest-mock-directly-above-jest-mocked': 'error',
     },
     parserOptions: {
         ecmaVersion: "latest"
     },
-
     env: {
         es6: true
     },
